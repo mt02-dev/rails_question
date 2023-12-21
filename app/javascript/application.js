7c1717054e33ae2c41c977cbddd7c17a04e0c37a
mt02-dev <t.m.215soccer@icloud.com>
2023-12-21 13:34:28 +0900

    turbo install, stimulus install 実行

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "popper"
 import "bootstrap"
-
+import "@hotwired/turbo-rails"
 window.onload = function () {
   const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
   const popoverList = [...popoverTriggerList].map(d => new bootstrap.Popover(d))
