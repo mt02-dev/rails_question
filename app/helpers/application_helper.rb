60fdfa69424e341094cdc6e8c92a7df5d34a19ae
mt02-dev <t.m.215soccer@icloud.com>
2023-12-27 14:47:51 +0900

    bootstrapのバージョン差異により動作しない点を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,17 @@ module ApplicationHelper
   # path: リンク先のパス
   def header_link_item(name,path)
     # bootstrapのデザインに合わせるため必ずつけるクラス
-    class_name = 'nav-item'
+    # class_name = 'nav-item'
     # activeの前には必ずスペースが必要(class="nav-itev active"となる)
     # current_page?メソッドで表示するパスと引数のパスが同一かを判定しするヘルパー
+    # v5.0以降 'nav-item'に'active'つけても反応しないらしい
+    class_name = 'nav-link'
     class_name << ' active' if current_page?(path)
 
     # liタグを作成、ブロック内に記載したHTMLはタグの中に展開される
     # 今回の場合はliタグの中にaタグが埋め込まれて呼び出し先に返される
-    content_tag :li, class: class_name do 
-      link_to name, path, class: 'nav-link'
+    content_tag :li, class: 'nav-item' do 
+      link_to name, path, class: class_name
     end
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,9 +2,11 @@
 import "popper"
 import "bootstrap"
 import "@hotwired/turbo-rails"
+
 window.onload = function () {
   const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
   const popoverList = [...popoverTriggerList].map(d => new bootstrap.Popover(d))
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
   const tooltipList = [...tooltipTriggerList].map(d => new bootstrap.Tooltip(d))
 }
+
