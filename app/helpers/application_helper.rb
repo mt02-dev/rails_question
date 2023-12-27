02bad0f5ce2c5fdddc823f9fbfa730118c2b4ad8
mt02-dev <t.m.215soccer@icloud.com>
2023-12-27 13:54:34 +0900

    header 追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # name: リンクの表示名
+  # path: リンク先のパス
+  def header_link_item(name,path)
+    # bootstrapのデザインに合わせるため必ずつけるクラス
+    class_name = 'nav-item'
+    # activeの前には必ずスペースが必要(class="nav-itev active"となる)
+    # current_page?メソッドで表示するパスと引数のパスが同一かを判定しするヘルパー
+    class_name << ' active' if current_page?(path)
+
+    # liタグを作成、ブロック内に記載したHTMLはタグの中に展開される
+    # 今回の場合はliタグの中にaタグが埋め込まれて呼び出し先に返される
+    content_tag :li, class: class_name do 
+      link_to name, path, class: 'nav-link'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'home#index'
   resources :boards
   resources :comments, only: %i[create destroy]
 end
