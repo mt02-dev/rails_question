e95de50cff83eb38cd4fe4bf9f2dbdd5ebfccc6e
mt02-dev <t.m.215soccer@icloud.com>
2024-01-09 14:08:36 +0900

    usersテーブルにbirthdayカラム追加、テスト用ファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_065233) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_09_050323) do
   create_table "board_tag_relations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "board_id", null: false
     t.bigint "tag_id", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_065233) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "birthday"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
